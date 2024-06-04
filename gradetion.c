5894015304625cadb7e450df2c3e8375e6e4a97d
SamantaX-31 <samanta.shuvajit2005@gmail.com>
2024-06-04 18:17:23 +0530

    Updated

--- gradetion.c
+++ gradetion.c
@@ -1,47 +1,54 @@
-#include<stdio.h>
-int main()
-{
-    int Marks;
-    printf("Enter Your Marks:");
-    scanf("%d", &Marks);
+#include <stdio.h>
 
-    switch(Marks)
+int main() {
+    float eng, ben, chem, math, comp;
+    float total, percentage;
+
+    printf("Enter your English Marks:\n");
+    scanf("%f", &eng);
+    
+    printf("Enter your Bengali Marks:\n");
+    scanf("%f", &ben);
+    
+    printf("Enter your Chemistry Marks:\n");
+    scanf("%f", &chem);
+    
+    printf("Enter your Mathematics Marks:\n");
+    scanf("%f", &math);
+    
+    printf("Enter your Computer Marks:\n");
+    scanf("%f", &comp);
+
+    total = eng + ben + chem + math + comp;
+    percentage = (total / 500.0) * 100;
+
+    printf("Total marks = %f\n", total);
+    printf("Percentage = %f\n", percentage);
+    
+    if(percentage>=90)
     {
-        case 1:
-        if(Marks <=90 || Marks>=100)
-        { 
-            printf("Your Grade is A\n");
-            break;
-        }
-
-      /*  case 1:
-        if(Marks <=90 || Marks>=100)
         printf("Your Grade is A\n");
-        break;
-
-        case 1:(Marks <=90 || Marks>=100)
-        printf("Your Grade is A\n");
-        break;
-
-        case 2:(Marks <=80 || Marks>=90)
-        printf("Your Grade is B\n");
-        break;
-
-        case 3:(Marks <=70 || Marks>=80)
-        printf("Your Grade is C\n");
-        break;
-
-        case 4:(Marks <=60 || Marks>=70)
-        printf("Your Grade is D\n");
-        break;
-
-        case 5:(Marks <=70)
-        printf("Your Grade is F\n");
-        break;
-
-        default:
-        printf("Invalid Grade\n");
-        break;
     }
-    */
+    
+    else if(percentage>=80)
+    {
+        printf("Your Grade is B\n");
+    }
+    
+    else if(percentage>=70)
+    {
+        printf("Your Grade is C\n");
+    }
+    
+    else if(percentage>=60)
+    {
+        printf("Your Grade is D\n");
+    }
+    
+    else
+    {
+        printf("Your Grade is F\n");
+    }
+
+    return 0;
 }
