e98d31f6a4b29697abd4f2c01eed572933f0e7e0
SamantaX-31 <samanta.shuvajit2005@gmail.com>
2024-06-04 18:25:01 +0530

    Updated

---
+++ Celsius_to_fahrenheit.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    float Celsius,Fahrenheit,a=5/9;
+    printf("Enter your temp. in celsius:\n");
+    scanf("%f",&Celsius);
+    printf("The temp. in Fahrenheit is %f",a*Fahrenheit-32);
+    return 0;
+}
